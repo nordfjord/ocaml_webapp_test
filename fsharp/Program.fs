f2f86c314d8040c006de31c8f55c3a40210f1ee8
Einar Norðfjörð <einar@nordfjord.io>
2023-04-03 22:13:07 -0400

    fsharp

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -1,18 +1,21 @@
 open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
-open Dapper
+open Microsoft.Extensions.Logging
+open System.Text.Json
 
-[<CLIMutable>]
-type Message =
+type Message<'Format> =
   { stream_name: string
     position: int64
-    data: ReadOnlyMemory<byte>
-    meta: ReadOnlyMemory<byte>
+    data: 'Format
+    meta: 'Format
     time: DateTimeOffset
     message_type: string }
 
 module Db =
+  open Npgsql
+  open NpgsqlTypes
+
   let connect () =
     task {
       let conn =
@@ -25,24 +28,50 @@ module Db =
   let get_category_messages (categoryName: string) (position: int64) =
     task {
       use! conn = connect ()
+      use cmd = conn.CreateCommand()
 
-      return!
-        conn.QueryAsync<Message>(
-          "SELECT stream_name, position, time, data, metadata AS meta, type AS message_type 
-           FROM get_category_messages(@CategoryName, @Position)",
-          {| CategoryName = categoryName
-             Position = position |}
+      cmd.CommandText <-
+        "SELECT stream_name, position, time, data, metadata AS meta, type AS message_type 
+           FROM get_category_messages(@CategoryName, @Position)"
+
+      cmd.Parameters.AddWithValue("CategoryName", NpgsqlDbType.Text, box categoryName) |> ignore
+      cmd.Parameters.AddWithValue("Position", NpgsqlDbType.Bigint, box position) |> ignore
+
+      use! reader = cmd.ExecuteReaderAsync()
+
+      let result = ResizeArray()
+
+      while reader.Read() do
+        result.Add(
+          { stream_name = reader.GetString(0)
+            position = reader.GetInt64(1)
+            time = DateTimeOffset(reader.GetDateTime(2))
+            data =
+              (if reader.IsDBNull(3) then
+                 JsonSerializer.Deserialize<JsonElement>("null")
+               else
+                 JsonSerializer.Deserialize(reader.GetString(3)))
+            meta =
+              (if reader.IsDBNull(4) then
+                 JsonSerializer.Deserialize<JsonElement>("null")
+               else
+                 JsonSerializer.Deserialize(reader.GetString(4)))
+            message_type = reader.GetString(5) }
         )
+
+      return result
+
     }
 
 let builder = WebApplication.CreateBuilder()
+builder.Logging.AddFilter("Microsoft.AspNetCore", LogLevel.Warning) |> ignore
 let app = builder.Build()
 
 app.MapGet(
   "/category/{category_name}/{position}",
-  Func<_, _, _>(fun categoryName position ->
+  Func<_, _, _>(fun category_name position ->
     task {
-      let! rows = Db.get_category_messages categoryName position
+      let! rows = Db.get_category_messages category_name position
       return rows
     })
 )
