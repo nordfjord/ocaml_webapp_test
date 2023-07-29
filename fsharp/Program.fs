6741cecd72f6fb5d0eb4bf053bfa10078df27353
Einar Norðfjörð <gloogankle@gmail.com>
2023-07-29 16:37:07 -0400

    simplify f#

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -3,6 +3,8 @@ open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 open System.Text.Json
+open System.Text.Json.Nodes
+open System.Threading.Tasks
 
 type Message<'Format> =
   { stream_name: string
@@ -12,54 +14,59 @@ type Message<'Format> =
     time: DateTimeOffset
     message_type: string }
 
+module Json =
+  let read struct (reader: System.Data.Common.DbDataReader, index: int) : ValueTask<JsonElement> =
+    if reader.IsDBNull(index) then
+      ValueTask.FromResult(JsonSerializer.Deserialize<JsonElement>("null"))
+    else
+      reader.GetStream(index) |> JsonSerializer.DeserializeAsync<JsonElement>
+
+module GetCategoryMessages =
+  [<Literal>]
+  let Query = "SELECT stream_name, position, time::text, data, metadata, type FROM get_category_messages($1, $2, 10)"
+
+  let prepare struct (dataSource: Npgsql.NpgsqlDataSource, categoryName: string, position: int64) =
+    let cmd = dataSource.CreateCommand(Query)
+    cmd.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Text, categoryName) |> ignore
+    cmd.Parameters.AddWithValue(NpgsqlTypes.NpgsqlDbType.Bigint, position) |> ignore
+    cmd
+  
+  let read (reader: System.Data.Common.DbDataReader) = task {
+    let data = Json.read (reader, 3)
+    let meta = Json.read (reader, 4)
+    let! data = data
+    let! meta = meta
+    return {
+      stream_name = reader.GetString(0)
+      position = reader.GetInt64(1)
+      time = DateTimeOffset.Parse(reader.GetString(2))
+      data = data
+      meta = meta
+      message_type = reader.GetString(5) } }
+
+
 module Db =
   open Npgsql
   open NpgsqlTypes
 
-  let connect () = task {
-    let conn =
-      new Npgsql.NpgsqlConnection("Host=localhost; Port=5432; Database=message_store; Username=message_store; Maximum Pool Size=10")
-
-    do! conn.OpenAsync()
-    return conn }
-
+  let dataSource = 
+    NpgsqlDataSource.Create("Host=localhost; Port=5432; Database=message_store; Username=message_store; Maximum Pool Size=10")
+  
   let get_category_messages (categoryName: string) (position: int64) = task {
-    use! conn = connect ()
-    use cmd = conn.CreateCommand()
-
-    cmd.CommandText <-
-      "SELECT stream_name, position, time, data, metadata AS meta, type AS message_type 
-         FROM get_category_messages($1, $2, 10)"
-
-    cmd.Parameters.AddWithValue(NpgsqlDbType.Text, box categoryName) |> ignore
-    cmd.Parameters.AddWithValue(NpgsqlDbType.Bigint, box position) |> ignore
+    use cmd = GetCategoryMessages.prepare (dataSource, categoryName, position)
 
     use! reader = cmd.ExecuteReaderAsync()
 
-    let result = ResizeArray()
+    let result = ResizeArray(10)
 
     while reader.Read() do
-      result.Add(
-        { stream_name = reader.GetString(0)
-          position = reader.GetInt64(1)
-          time = DateTimeOffset(reader.GetDateTime(2))
-          data =
-            (if reader.IsDBNull(3) then
-               JsonSerializer.Deserialize<JsonElement>("null")
-             else
-               JsonSerializer.Deserialize(reader.GetString(3)))
-          meta =
-            (if reader.IsDBNull(4) then
-               JsonSerializer.Deserialize<JsonElement>("null")
-             else
-               JsonSerializer.Deserialize(reader.GetString(4)))
-          message_type = reader.GetString(5) }
-      )
+      let! message = GetCategoryMessages.read reader
+      result.Add(message)
 
     return result }
 
 let builder = WebApplication.CreateBuilder()
-builder.Logging.AddFilter("Microsoft.AspNetCore", LogLevel.Warning) |> ignore
+builder.Logging.SetMinimumLevel(LogLevel.Warning) |> ignore
 let app = builder.Build()
 
 app.MapGet(
--- fsharp/README.md
+++ fsharp/README.md
@@ -15,23 +15,23 @@
            * default: 80 looping VUs for 10s (gracefulStop: 30s)
 
 
-running (10.0s), 00/80 VUs, 28902 complete and 0 interrupted iterations
-default ✓ [========================] 80 VUs  10s
+running (10.0s), 00/80 VUs, 35720 complete and 0 interrupted iterations
+default ✓ [=====================================] 80 VUs  10s
 
-     data_received..................: 99 MB  9.8 MB/s
-     data_sent......................: 3.2 MB 314 kB/s
-     http_req_blocked...............: avg=11.96µs min=0s      med=1µs     max=4.69ms   p(90)=1µs     p(95)=1µs
-     http_req_connecting............: avg=5.03µs  min=0s      med=0s      max=2.06ms   p(90)=0s      p(95)=0s
-     http_req_duration..............: avg=27.68ms min=18.68ms med=26.95ms max=206.19ms p(90)=29.68ms p(95)=30.73ms
-       { expected_response:true }...: avg=27.68ms min=18.68ms med=26.95ms max=206.19ms p(90)=29.68ms p(95)=30.73ms
-     http_req_failed................: 0.00%  ✓ 0           ✗ 28902
-     http_req_receiving.............: avg=13.54µs min=4µs     med=12µs    max=401µs    p(90)=17µs    p(95)=25µs
-     http_req_sending...............: avg=2.59µs  min=1µs     med=2µs     max=134µs    p(90)=3µs     p(95)=4µs
+     data_received..................: 122 MB 12 MB/s
+     data_sent......................: 3.9 MB 388 kB/s
+     http_req_blocked...............: avg=9.41µs  min=0s      med=1µs     max=4.51ms   p(90)=1µs     p(95)=1µs
+     http_req_connecting............: avg=4.17µs  min=0s      med=0s      max=2.12ms   p(90)=0s      p(95)=0s
+     http_req_duration..............: avg=22.39ms min=15.4ms  med=21.2ms  max=200.06ms p(90)=26.86ms p(95)=27.81ms
+       { expected_response:true }...: avg=22.39ms min=15.4ms  med=21.2ms  max=200.06ms p(90)=26.86ms p(95)=27.81ms
+     http_req_failed................: 0.00%  ✓ 0           ✗ 35720
+     http_req_receiving.............: avg=14.54µs min=4µs     med=12µs    max=1.52ms   p(90)=21µs    p(95)=37µs
+     http_req_sending...............: avg=2.8µs   min=1µs     med=2µs     max=292µs    p(90)=3µs     p(95)=6µs
      http_req_tls_handshaking.......: avg=0s      min=0s      med=0s      max=0s       p(90)=0s      p(95)=0s
-     http_req_waiting...............: avg=27.67ms min=18.66ms med=26.94ms max=206.17ms p(90)=29.66ms p(95)=30.7ms
-     http_reqs......................: 28902  2883.117046/s
-     iteration_duration.............: avg=27.71ms min=18.73ms med=26.96ms max=210.25ms p(90)=29.7ms  p(95)=30.74ms
-     iterations.....................: 28902  2883.117046/s
+     http_req_waiting...............: avg=22.38ms min=15.39ms med=21.18ms max=200.04ms p(90)=26.85ms p(95)=27.79ms
+     http_reqs......................: 35720  3562.192927/s
+     iteration_duration.............: avg=22.42ms min=15.41ms med=21.21ms max=203.86ms p(90)=26.88ms p(95)=27.82ms
+     iterations.....................: 35720  3562.192927/s
      vus............................: 80     min=80        max=80
      vus_max........................: 80     min=80        max=80
 ```
